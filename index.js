cd8ce65f008ea657b08b5c6dc9dd344c1f3b472a
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-08-30 22:28:27 -0300

    valida si ruta no es valida

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 const colors = require('colors');
 const Marked = require('marked');
-let numLine;
+let numLine=[];
 
 const [, , ...userCLIArgs] = process.argv;
 
@@ -12,14 +12,29 @@ function readFilePromise(filePath) {
   return new Promise((reject) => {
     fs.readFile(filePath, 'utf-8', (error, data) => {
       if (error) {
+
+
+        let validatePath=filePath.substr(filePath.length - 3);
+
+        if (validatePath!='.md'){
+          console.log('ingrese archivo valido')
+          return reject(error);
+         
+        }
+    
+
         return reject(error);
       }
       markdownLinkExtractor(data,numLine);
       
+   
+     
     });
   });
 }
 readFilePromise(userCLIArgs[0]).then(() => {
+
+
 
 }).catch((error) => {
   console.error('Error > ' + error);
@@ -31,13 +46,18 @@ readFilePromise(userCLIArgs[0]).then(() => {
 // Recibe texto en markdown y retorna sus links en un arreglo
 function markdownLinkExtractor(markdown) {
 
+
+
   let text = fs.readFileSync(userCLIArgs[0]).toString(); // lee todo el archivo
   let lines = text.split('\n');
-
+  console.log(userCLIArgs[0])
   lines.map(element => {
     numLine = (lines.indexOf(element) + 1);
-    console.log(numLine + '..............' + element);
+    //console.log(numLine+ '..............' + element);
+
+    
   });
+
   
   const links = [];
 
@@ -50,22 +70,22 @@ function markdownLinkExtractor(markdown) {
   Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
   Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
 
-  renderer.link = function(href, title, text, numLine) {
+  renderer.link = function(href, title, text, line) {
     links.push({
       href,
       text,
       title,
-      numLine
+      line:line
     });
   };
-  renderer.image = function(href, title, text,numLine) {
+  renderer.image = function(href, title, text,line) {
     // Remove image size at the end, e.g. ' =20%x50'
     href = href.replace(/ =\d*%?x\d*%?$/, '');
     links.push({
       href,
       text,
       title,
-      numLine
+      line:line
     });
   };
   Marked(markdown, { renderer });
@@ -73,14 +93,14 @@ function markdownLinkExtractor(markdown) {
   links.forEach((element) => { // busca dentro del objeto links
     const url = element.href;
     const txt = element.text;
-    const line = element.numline;
+    const line = element.line;
 
     fetch(url).then(response => response).then((data) => {
       validate = {
         'text': txt,
         'url': url,
         'status': data.statusText + ' ' + data.status,
-        'linea': line,
+        'linea': line
       };
 
       // if (data.status >= 400 && data.status <= 499) {
